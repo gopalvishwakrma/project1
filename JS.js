a806d8a79d139622a609ac6774232b2b2f2475df
Gopal Vishwakarma <123076194+gopalvishwakrma@users.noreply.github.com>
2023-09-14 18:01:32 +0530

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,9 @@
+document.getElementById("loginBtn").addEventListener("click", function () {
+    // Redirect to login.html when the Login button is clicked
+    window.location.href = "login.html";
+});
+
+document.getElementById("signupBtn").addEventListener("click", function () {
+    // Redirect to signup.html when the Sign up button is clicked
+    window.location.href = "signup.html";
+});
